5d3a6d75df61d12bbd2852fc6ce05f1ad5809d01
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-07-21 15:24:52 -0700

    react-1-afternoon

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class EvenAndOdd extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: '',
+            handleChange(val){
+                this.setState({userInput: val});
+            }
+        }
+    }
+
+
+    render(){
+        return(
+            <div class="puzzleBox evenAndOddPB">
+                <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value)}/>
+                <button className="confirmationButton" onClick={ () => {this.assignEvenAndOdds(this.state.userInput)}}>Split</button>
+                <span className="resultsBox"> Evens: {JSON.stringify(this.state.evenArray)}</span>
+                <span className="resultsBox"> Odds: {JSON.stringify(this.state.oddArray)}</span>
+            </div>
+        )
+    }
+}
+
+export default EvenAndOdd
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FilterObjects extends Component{
+    render(){
+        return(
+            <p>FilterObjects Component</p>
+        )
+    }
+}
+
+export default FilterObjects
