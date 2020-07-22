95ed843cc6c66e39a3e08744e4c6b11a28c8eb76
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-07-21 22:08:27 -0700

    react-1-afternoon

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 
 class FilterObjects extends Component{
+    constructor(){
+        super()
+
+
+        this.state ={
+            teammates: [
+                {
+                    name: 'Cam Jordan',
+                    title: 'Edge Rusher',
+                    age: 31
+                },{
+                    name: 'Alvin Kamara',
+                    title: 'Running Back',
+                    age: 25
+                },{
+                    name: 'Drew Brees',
+                    title: 'quarterBack',
+                    age: 41
+                }],
+
+                userInput: '',
+                filteredEmployee: []
+            }
+        }
+
+
     render(){
         return(
-            <p>FilterObjects Component</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText">Original: { JSON.stringify(this.state.teammates, null, 10) }</span>
+                <input className="inputLIne" onChange={ (e) => this.handleChange(e.target.value) }/>
+                <button className="confirmationButton"></button>
+                <span className="resultBox filterObjectRB">Filtered: {JSON.stringify(this.state.filteredTeammates, null, 10) }</span>
+            </div>
         )
     }
 }
